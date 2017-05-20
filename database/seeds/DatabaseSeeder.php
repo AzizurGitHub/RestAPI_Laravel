655de6a0491ea4df27e59b2b3cf341261eae0d91
AzizurGitHub <azizurfreelancer123@gmail.com>
2017-05-20 20:51:38 +0600

    db migration and seed complete

---
+++ database/seeds/DatabaseSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+use App\Category;
+use App\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+//        // $this->call(UsersTableSeeder::class);
+//        DB::statement('SET FOREIGN_KEY_CHECKS=0');
+//
+//
+//        $table = [
+//            'users',
+//            'categories',
+//            'products',
+//            'transactions',
+//            'category_product'
+//        ];
+//
+//        foreach ($table as $item) {
+//            DB::table($item)->truncate();
+//        }
+//
+//
+//
+//        DB::statement('SET FOREIGN_KEY_CHECKS=1');
+//
+//        factory(User::class,200)->create();
+//        factory(\App\Category::class,30)->create();
+//        factory(\App\Product::class,1000)->create()->each(function ($product){
+//            $categories = Category::all()->random(mt_rand(1,5))->pluck('id');
+//            $product->categories()->attach($categories);
+//        });
+
+        factory(\App\Transaction::class,1000)->create();
+
+
+    }
+}
